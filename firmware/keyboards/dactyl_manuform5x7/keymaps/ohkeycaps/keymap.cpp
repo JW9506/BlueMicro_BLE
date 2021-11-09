e9f457baf6e394c51a6e6c08b734a8fe2c8aa4c0
JW9506 <cf.dota@gmail.com>
2021-11-09 16:10:06 -0500

    fix

--- firmware/keyboards/dactyl_manuform5x7/keymaps/ohkeycaps/keymap.cpp
+++ firmware/keyboards/dactyl_manuform5x7/keymaps/ohkeycaps/keymap.cpp
@@ -94,7 +94,7 @@ void setupKeymap() {
  * ,------------------------------------------------.
  * |  F12 |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |
  * |-----------------------------------------|------|
- * |      |      |      |      |      |      |      |
+ * |      | print|      |      |      |      |      |
  * |------+------+------+------+------+------|------|
  * |      | GUI  | Alt  | Shift| Ctrl |      |      |
  * |------+------+------+------+------+------|------'
@@ -217,7 +217,7 @@ uint32_t colemak[MATRIX_ROWS][MATRIX_COLS] =
 	    KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,   KC_HOME,
         KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_END,
         KC_EQUAL, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,   XXXXXXX,
-        _______, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,
+        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,
         KC_LEFT, KC_GRAVE, KC_QUOTE, KC_RIGHT,
                                             KC_BSPC, KC_LSFT,
                                             KC_LCTL, KC_LALT,
