d227c1c6998d64318262e8d4e1c9383102c93d19
JW9506 <cf.dota@gmail.com>
2021-11-09 17:28:53 -0500

    num pad and f keys

--- firmware/keyboards/dactyl_manuform5x7/keymaps/ohkeycaps/keymap.cpp
+++ firmware/keyboards/dactyl_manuform5x7/keymaps/ohkeycaps/keymap.cpp
@@ -125,9 +125,9 @@ void setupKeymap() {
 
 /* Raise
  * ,------------------------------------------------.
- * |   ~  |  !   |  @   |  #   |  $   |  %   |      |
+ * |   ~  |  F1   |  F2 |  F3  |  F4  |  F5  |      |
  * |-----------------------------------------|------|
- * | Mute | Home | PgDn | PgUp | End  |      |  (   |
+ * | Mute | Home | PgDn | ESC | End  |      |  (   |
  * |------+------+------+------+------+------|------|
  * | Vol+ | Left | Down |  Up  | Right|      |      |
  * |------+------+------+------+------+------|------'
@@ -144,8 +144,8 @@ void setupKeymap() {
  */
     uint32_t raise[MATRIX_ROWS][MATRIX_COLS] =
         KEYMAP( \
-  KC_TILD,KC_EXLM,KC_AT,  KC_HASH,KC_DLR, KC_PERC, _______, \
-  KC_MUTE,KC_HOME,KC_PGDN,KC_PGUP,KC_END ,_______, KC_LPRN, \
+  KC_TILD,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5, _______, \
+  KC_MUTE,KC_HOME,KC_PGDN,KC_ESC, KC_END, _______, KC_LPRN, \
   KC_VOLU,KC_LEFT,KC_DOWN,KC_UP,  KC_RGHT,_______, _______, \
   KC_VOLD,_______,_______,_______,_______,_______, \
   _______,_______,_______,_______,                 \
@@ -336,11 +336,11 @@ void setupKeymap() {
  *         ,------------------------------------------------.
  *         |      |   F6 |  F7  |  F8  |  F9  | F10  | F11  |
  *         |------------------------------------------------|
- *         |      | PgUp | Home |  Up  | End  | Del  |      |
+ *         |      | PgUp | 7    | 8    | 9  | Del  |      |
  *         |------+------+------+------+------+-------------|
- *         |      | PgDn | Left | Down | Right| BkSp |      |
+ *         |      | PgDn | 4    | 5    | 6   | BkSp |      |
  *         `------+------+------+------+------+------|------|
- *                |  Ins |      |      |      |      |      |
+ *                |  Ins | 1    | 2    | 3    | 0     |      |
  *                `-----------------------------------------'
  *                              |      |      |      |      |
  *                ,-----------------------------------------'
@@ -354,9 +354,9 @@ void setupKeymap() {
     uint32_t lower[MATRIX_ROWS][MATRIX_COLS] =
         KEYMAP( \
    KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11, KC_F12,  _______, \
-   _______,KC_PGUP, KC_HOME, KC_UP,   KC_END,  KC_DEL,  _______,  \
-   _______,KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_BSPC, _______,  \
-           KC_INS , XXXXXXX, _______, _______, _______, KC_RSFT,  \
+   _______,KC_PGUP, KC_7, KC_8, KC_9,  KC_DEL,  _______,  \
+   _______,KC_PGDN, KC_4, KC_5, KC_6, KC_BSPC, _______,  \
+           KC_INS , KC_1, KC_2, KC_3,  KC_0, KC_RSFT,  \
                              _______, _______, _______, _______,  \
            _______, _______,                                      \
            _______, _______,                                      \
